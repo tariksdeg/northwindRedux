dde73cb853287e563ab30f3031dab858f68b469f
tariksdeg <tariksdegirmencii@gmail.com>
2022-12-28 17:47:45 +0300

    RemoveFromCart

--- northwind-redux/src/components/cart/CartSummary.js
+++ northwind-redux/src/components/cart/CartSummary.js
@@ -7,49 +7,59 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  NavItem,NavLink, Badge
+  NavItem,
+  NavLink,
+  Badge,
 } from "reactstrap";
+import * as cartActions from "../../redux/actions/cartActions";
 
 class CartSummary extends Component {
-
-  renderEmpty(){
-    return(<NavItem>
-    <NavLink>Sepetiniz Boş</NavLink>
-  </NavItem>)
-    
+  renderEmpty() {
+    return (
+      <NavItem>
+        <NavLink>Sepetiniz Boş</NavLink>
+      </NavItem>
+    );
   }
 
-  renderSummary(){
-return(
-   <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret>
-            Sepetiniz
-          </DropdownToggle>
-          
-            <DropdownMenu right>
-              {this.props.cart.map((cartItem) => (
-              <DropdownItem key={cartItem.product.id}>{cartItem.product.productName}-<Badge color="success">{cartItem.quantity}</Badge></DropdownItem>
-           ) )}
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-          
-        </UncontrolledDropdown>
-)
+  renderSummary() {
+    return (
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Sepetiniz
+        </DropdownToggle>
+
+        <DropdownMenu right>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              <Badge color="danger"onClick={()=>this.props.actions.removeFromCart(cartItem.product)} >X</Badge>
+              {cartItem.product.productName}-
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
   }
   render() {
     return (
       <div>
-        {
-          this.props.cart.length>0?this.renderSummary():this.renderEmpty()
-        }
-       
+        {this.props.cart.length > 0 ? this.renderSummary() : this.renderEmpty()}
       </div>
     );
   }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      removeFromCart: bindActionCreators(cartActions.removeFromCart, dispatch),
+    },
+  };
 }
 function mapStateToProps(state) {
   return { cart: state.cartReducer };
 }
 
-export default connect(mapStateToProps)(CartSummary);
+export default connect(mapStateToProps, mapDispatchToProps)(CartSummary);
