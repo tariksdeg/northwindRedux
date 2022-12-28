ccbc23fcc2d43c0d1abbc321690b6eaad5ac5a64
tariksdeg <tariksdegirmencii@gmail.com>
2022-12-28 17:23:10 +0300

    RemoveCart

--- northwind-redux/src/components/navi/Navi.js
+++ northwind-redux/src/components/navi/Navi.js
@@ -1,59 +1,50 @@
-import React, { Component , useState} from 'react'
+import React, { Component } from "react";
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-  } from 'reactstrap';
-  
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+ 
+} from "reactstrap";
+import CartSummary from "../cart/CartSummary";
 
-  
+
 export default class Navi extends Component {
-    
-        constructor(props) {
-          super(props);
-          this.toogle = this.toogle.bind(this);
-          this.state = {
-            isOpen: false,
-          };
-        }
-        toogle() {
-          this.setState({
-            isOpen: !this.state.isOpen,
-          });
-        }
-      
-    render() {
-        return (
-            <div>
-              <Navbar color="light" light expand="md">
+  constructor(props) {
+    super(props);
+    this.toogle = this.toogle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toogle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
           <NavbarBrand href="/">APP</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="m1-auto" navbar>
               <NavItem>
-                <NavLink>
-                  
-                </NavLink>
+                <NavLink></NavLink>
               </NavItem>
               <NavItem>
-                <NavLink>
-                  
-                </NavLink>
+                <NavLink></NavLink>
               </NavItem>
-              
+              <CartSummary/>
             </Nav>
           </Collapse>
         </Navbar>
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
